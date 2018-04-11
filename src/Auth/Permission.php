29daf58eaccb453d21a0cf2c30302d977055af98
Dan <k.daniel.adams@gmail.com>
2018-04-11 09:15:55 -0400

    fix up role and permission classes

--- src/Auth/Permission.php
+++ src/Auth/Permission.php
@@ -8,19 +8,40 @@ use Olorin\Mgmt\MgmtModel;
 
 class Permission extends MgmtModel
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name', 'label'
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = [
         'created_at'
     ];
 
+    /**
+     * Specify many-to-many relationship with App\Role::class.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function roles()
     {
         return $this->belongsToMany(Role::class);
     }
 
+    /**
+     * Define some properties for displaying this model's fields
+     * in the MGMT editor.
+     *
+     * @return array|mixed
+     */
     public function getMgmtFieldsAttribute()
     {
         parent::getMgmtFieldsAttribute();
